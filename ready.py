378d06708200bd6c725f19ba0da4a5ee8636f2cc
Luke Stebbing <luke@elicit.com>
2025-07-26 11:10:57 -0700

    For now, mixed mode at startup

--- ready.py
+++ ready.py
@@ -1,8 +1,9 @@
 from talon import actions, app
 
+
 def on_ready():
-    actions.speech.disable()
     actions.mode.enable("command")
     actions.mode.enable("dictation")
+
 
 app.register("ready", on_ready)
